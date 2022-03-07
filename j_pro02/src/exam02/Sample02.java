6423c4b63d3018cd86f44236190de41c222204ea
안병현 <abhyuni@gmail.com>
2022-03-07 12:42:06 +0900

    printf 정렬

--- j_pro02/src/exam02/Sample02.java
+++ j_pro02/src/exam02/Sample02.java
@@ -3,8 +3,37 @@ package exam02;
 public class Sample02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		 * format 활용
+		 */
+		
+		// 정렬(왼쪽 또는 오른쪽)
+		System.out.printf("%5s | %5s\n", "name", "age");
+		System.out.printf("%5s | %5d\n", "Hong", 32);
+		System.out.printf("%5s | %5d\n", "Kim", 27);
+		System.out.printf("%5s | %5d\n", "Park", 29);
+		
+		System.out.println("--------------------");
+		
+		System.out.printf("%-5s | %-5s\n", "name", "age");
+		System.out.printf("%-5s | %-5d\n", "Hong", 32);
+		System.out.printf("%-5s | %-5d\n", "Kim", 27);
+		System.out.printf("%-5s | %-5d\n", "Park", 29);
+		
+		System.out.println("--------------------");
+		
+		System.out.printf("%-5s | %-5s\n", "name", "age");
+		System.out.printf("%-5s | %-5.1f\n", "Hong", 32.0);
+		System.out.printf("%-5s | %-5.1f\n", "Kim", 27.1);
+		System.out.printf("%-5s | %-5.1f\n", "Park", 29.2);
+		
+		System.out.println("--------------------");
+		
+		// 한글을 사용할 때 문제점...
+		System.out.printf("%6s | %5s\n", "이름", "나이");	// 한글은 2자리 차지...
+		System.out.printf("%6s | %5d\n", "홍길동", 32);
+		System.out.printf("%6s | %5d\n", "김건", 27);
+		System.out.printf("%6s | %5d\n", "박지우", 29);
 	}
 
 }
