7202d79998cd48c98dafe76a674aa288c45a7975
안병현 <abhyuni@gmail.com>
2022-03-11 10:49:59 +0900

    중첩반복문

--- j_pro03/src/exam02/Sample07.java
+++ j_pro03/src/exam02/Sample07.java
@@ -22,6 +22,23 @@ public class Sample07 {
 			}
 			System.out.print("\n");
 		}
+		
+		/*
+		 * 다음의 결과가 나올수 있도록 중첩반복문을 사용하시오.(띄어쓰기는 \t 를 사용하도록 한다.)
+		 * 
+		 * 1
+		 * 2    3
+		 * 4    5    6
+		 * 7    8    9    10
+		 * 11   12   13   14   15
+		 */
+		int n = 0;
+		for(int i = 1; i <= 5; i++) {
+			for(int j = 1; j <= i; j++) {
+				System.out.print(++n + "\t");
+			}
+			System.out.print("\n");
+		}
 	}
 
 }
