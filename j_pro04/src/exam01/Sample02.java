3d9fb92100f6aa213804388cb2495a007cbccde5
안병현 <abhyuni@gmail.com>
2022-03-14 13:47:34 +0900

    문자열 배열 / 배열의 초기값

---
+++ j_pro04/src/exam01/Sample02.java
@@ -0,0 +1,76 @@
+package exam01;
+
+public class Sample02 {
+
+	public static void main(String[] args) {
+		/*
+		 * 배열의 크기가 10인 배열을 선언하고 10 ~ 1 까지의 값으로
+		 * 초기화 하도록 한다.
+		 */
+		int[] arr1 = new int[10];
+		int init = 10;
+		
+		for(int i = 0; i < 10; i++) {
+			arr1[i] = init--;
+		}
+		
+		for(int i = 9; i >= 0; i--) {
+			arr1[i] = 10 - i;
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.print(arr1[i] + "\t");
+		}
+		System.out.print("\n");
+
+		/*
+		 * 배열의 크기가 10인 배열을 선언하고 2 부터 시작하여 짝수값에 해당하는
+		 * 값으로 초기화 하도록 한다.
+		 */
+		int[] arr2 = new int[10];
+		init = 2;
+		
+		for(int i = 0; i < 10; i++) {
+			arr2[i] = init;
+			init += 2;
+		}
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.print(arr2[i] + "\t");
+		}
+		System.out.print("\n");
+		
+		/*
+		 * 배열의 크기가 5인 실수 배열을 선언하고 1 부터 0.5 씩 증가된 값으로
+		 * 초기화 하도록 한다.
+		 */
+		double[] arr3 = new double[5];
+		double init2 = 1.0;
+		
+		for(int i = 0; i < 5; i++) {
+			arr3[i] = init2;
+			init2 += 0.5;
+		}
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.print(arr3[i] + "\t");
+		}
+		System.out.print("\n");
+		
+		/*
+		 * 배열의 크기가 5인 문자 배열을 선언하고 'A' ~ 'E' 까지의 문자값으로
+		 * 초기화 하도록 한다.
+		 */
+		char[] arr4 = new char[5];
+		
+		for(int i = 0; i < 5; i++) {
+			arr4[i] = (char)('A' + i);
+		}
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.print(arr4[i] + "\t");
+		}
+		System.out.print("\n");
+	}
+
+}
