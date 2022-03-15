3ed0159946e7fe9c662884a85442ea53cee48f7f
안병현 <abhyuni@gmail.com>
2022-03-15 11:53:41 +0900

    동적 배열 문제

--- j_pro04/src/exam01/Sample10.java
+++ j_pro04/src/exam01/Sample10.java
@@ -1,5 +1,8 @@
 package exam01;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Sample10 {
 
 	public static void main(String[] args) {
@@ -18,6 +21,35 @@ public class Sample10 {
 		 * 		총 합 : 43
 		 * 		평 균 : 14.3
 		 */
+		Scanner sc = new Scanner(System.in);
+		int cnt = 1;
+		int[] arr1 = new int[0];
+		while(true) {
+			System.out.printf("%d 번째 정수값 입력 : ", cnt);
+			int num = sc.nextInt();
+			
+			if(num == -1) {
+				break;
+			}
+			
+			int[] temp = Arrays.copyOf(arr1, arr1.length + 1);
+			arr1 = temp;
+			
+			arr1[cnt - 1] = num;
+			cnt += 1;
+			
+			System.out.println(Arrays.toString(arr1));
+		}
+		
+		int tot = 0;
+		double avg;
+		for(int i = 0; i < arr1.length; i++) {
+			tot += arr1[i];
+		}
+		avg = (double)tot / arr1.length;
+		
+		System.out.println("총 합 : " + tot);
+		System.out.println("평 균 : " + avg);
 	}
 
 }
