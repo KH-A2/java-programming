700c0b83cced5d2e5f7c415c12b78a800432cca0
안병현 <abhyuni@gmail.com>
2022-03-15 09:22:33 +0900

    가위 바위 보에 배열 적용.

--- j_pro03/src/exam02/Sample12.java
+++ j_pro03/src/exam02/Sample12.java
@@ -6,13 +6,17 @@ import java.util.Random;
 public class Sample12 {
 
 	public static void main(String[] args) {
+		// 배열을 적용한다.
+		// 		- 컴퓨터가 생성한 정수 값을 가위, 바위, 보로 치환하기 위한 배열
+		//		- 전적 기록을 배열을 사용하여 관리할 수 있도록 한다.
+		
 		Scanner sc = new Scanner(System.in);
 		Random random = new Random();
 		
 		String name, player, sCom = "";
 		int computer;
-		int result = 0;		// 이기면 1, 지면 -1, 비기면 0
-		int wCnt = 0, dCnt = 0, lCnt = 0;
+		int result = 0;		// 지면 0, 비기면 1, 이기면 2
+		int[] record = new int[3];
 		
 		System.out.print("당신의 이름을 입력하세요 : ");
 		name = sc.nextLine();
@@ -31,32 +35,36 @@ public class Sample12 {
 			}
 			
 			if(player.equals("exit")) {
-				System.out.printf("%d전 %d승 %d무 %d패", wCnt + dCnt + lCnt, wCnt, dCnt, lCnt);
+				System.out.printf("%d전 %d승 %d무 %d패", record[0] + record[1] + record[2]
+						, record[2], record[1], record[0]);
 				break;
 			} else if(player.equals("가위")) {
 				if(computer == 0) {
-					result = 0;		dCnt++;
+					result = 1;
 				} else if(computer == 1) {
-					result = -1;	lCnt++;
+					result = 0;
 				} else if(computer == 2) {
-					result = 1;		wCnt++;
+					result = 2;
 				}
+				record[result]++;
 			} else if(player.equals("바위")) {
 				if(computer == 0) {
-					result = 1;		wCnt++;
+					result = 2;
 				} else if(computer == 1) {
-					result = 0;		dCnt++;
+					result = 1;
 				} else if(computer == 2) {
-					result = -1;	lCnt++;
+					result = 0;
 				}
+				record[result]++;
 			} else if(player.equals("보")) {
 				if(computer == 0) {
-					result = -1;	lCnt++;
+					result = 0;
 				} else if(computer == 1) {
-					result = 1;		wCnt++;
+					result = 2;
 				} else if(computer == 2) {
-					result = 0;		dCnt++;
+					result = 1;
 				}
+				record[result]++;
 			} else {
 				System.out.println("잘못 입력하였습니다.");
 				continue;
@@ -64,13 +72,13 @@ public class Sample12 {
 			
 			System.out.printf("컴퓨터 : %s\n%s : %s\n", sCom, name, player);
 			switch(result) {
-				case -1:
+				case 0:
 					System.out.println("졌습니다.");
 					break;
-				case 0:
+				case 1:
 					System.out.println("비겼습니다.");
 					break;
-				case 1:
+				case 2:
 					System.out.println("이겼습니다.");
 			}
 		}
