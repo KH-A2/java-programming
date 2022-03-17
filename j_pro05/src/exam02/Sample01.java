1968c1d34263bb7c8ea4e88d3d7c1c4b4c2e6489
안병현 <abhyuni@gmail.com>
2022-03-17 14:24:50 +0900

    객체

---
+++ j_pro05/src/exam02/Sample01.java
@@ -0,0 +1,12 @@
+package exam02;
+
+import exam01.*;
+
+public class Sample01 {
+
+	public static void main(String[] args) {
+		PersonPub p1 = new PersonPub();
+		PersonDef p2 = new PersonDef();
+	}
+
+}
---
+++ j_pro05/src/exam04/FinalStaticTest.java
@@ -0,0 +1,5 @@
+package exam04;
+
+public class FinalStaticTest {
+	public final static int CONSTSHARE = 20;
+}
