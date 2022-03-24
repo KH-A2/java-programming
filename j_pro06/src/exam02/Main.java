e9fc3bc9971fa55c30f02c4eadb5fa6873baf407
안병현 <abhyuni@gmail.com>
2022-03-24 10:50:20 +0900

    클래스 만들기 연습 3

---
+++ j_pro06/src/exam02/Main.java
@@ -0,0 +1,20 @@
+package exam02;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ReportGrade report = new ReportGrade("홍길동");
+		
+		double[] grades = new double[] {78.5, 79.3, 87.2, 93.8, 93.4};
+		String[] subjects = new String[] {"국어", "영어", "수학", "사회", "체육"};
+		
+		report.setGrades(grades);
+		report.setSubjects(subjects);
+		
+		report.setGrades("수학", 92.5);
+		System.out.println(Arrays.toString(report.getGrades()));
+	}
+
+}
