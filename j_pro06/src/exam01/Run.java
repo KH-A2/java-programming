9ae6cf54f6dd509e099bfbac8f0eaf0c8fbc2c49
안병현 <abhyuni@gmail.com>
2022-03-24 10:18:23 +0900

    실행용 클래스

--- j_pro06/src/exam01/Run.java
+++ j_pro06/src/exam01/Run.java
@@ -1,5 +1,7 @@
 package exam01;
 
+import java.util.Arrays;
+
 public class Run {
 
 	public static void main(String[] args) {
@@ -13,6 +15,16 @@ public class Run {
 		System.out.println(g1.getName());
 		System.out.println(g1.getScore());
 		System.out.println(g1.getRank());
+		
+		Lotto lot =  new Lotto();
+		lot.generate();
+		System.out.println(Arrays.toString(lot.getNumbers()));
+		
+		lot.generate(1);
+		System.out.println(Arrays.toString(lot.getNumbers()));
+		
+		lot.generate(10, 20);
+		System.out.println(Arrays.toString(lot.getNumbers()));
 	}
 
 }
