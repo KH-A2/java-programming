04f9ee9109f8ffddd54372da2bf685b882cee4fb
안병현 <abhyuni@gmail.com>
2022-04-01 15:20:51 +0900

    날짜 차이 구하는 문제 풀이 추가

--- j_pro08/src/exam01/Sample07.java
+++ j_pro08/src/exam01/Sample07.java
@@ -3,6 +3,7 @@ package exam01;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
 
@@ -34,7 +35,7 @@ public class Sample07 {
 			} else if(input.equals("e")) {
 				break;
 			}
-		};
+		}
 		System.out.println(Arrays.toString(dates));
 	}
 	
@@ -45,6 +46,51 @@ public class Sample07 {
 		 * 그 차를 구하는 코드를 작성한다.
 		 * (과거의 날짜는 D-Day -10일, 미래의 날짜는 D-Day 10일)
 		 */
+		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+		String input = "";
+		Date[] dates = new Date[1];
+		
+		while(true) {
+			System.out.print("년/월/일 형식의 날짜 입력(e를 입력하면 종료) : ");
+			input = sc.nextLine();
+			if(input.matches("\\d{4}/\\d{1,2}/\\d{1,2}")) {
+				try {
+					dates[0] = format.parse(input);
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+				break;
+			}
+		}
+		
+		Calendar c1 = Calendar.getInstance();
+		Calendar c2 = Calendar.getInstance();
+		c1.setTime(dates[0]);
+		try {
+			c2.setTime(format.parse(format.format(new Date())));	// 현재 날자의 시간은 00:00:00 으로 맞추기 위한 작업.
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(c1.getTime());
+		System.out.println(c2.getTime());
+		
+		int totalDate = 0;
+		
+		// .after()   .before()
+		if(c1.after(c2)) {
+			while(c1.compareTo(c2) != 0) {
+				c1.add(Calendar.DATE, -1);
+				totalDate++;
+			}
+		} else {
+			while(c1.compareTo(c2) != 0) {
+				c1.add(Calendar.DATE, 1);
+				totalDate--;
+			}
+		}
+		
+		System.out.println("D-Day " + totalDate);
 	}
 	
 	public void ex03() {
@@ -58,8 +104,8 @@ public class Sample07 {
 
 	public static void main(String[] args) {
 		Sample07 sample = new Sample07();
-		sample.ex01();
-		// sample.ex02();
+		// sample.ex01();
+		sample.ex02();
 		// sample.ex03();
 	}
 
