165b3cc941490c4cc7dd993afdf6d4e4abad9133
안병현 <abhyuni@gmail.com>
2022-04-01 11:35:50 +0900

    Wrapper 클래스 적용

--- j_pro08/src/exam01/Sample02.java
+++ j_pro08/src/exam01/Sample02.java
@@ -1,5 +1,6 @@
 package exam01;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sample02 {
@@ -20,6 +21,12 @@ public class Sample02 {
 		System.out.print("정수값 입력 : ");
 		String input = sc.nextLine();
 		String[] sArr = input.split(" ");
+		
+		int[] iArr = new int[sArr.length];
+		for(int i = 0; i < sArr.length; i++) {
+			iArr[i] = Integer.parseInt(sArr[i]);
+		}
+		System.out.println(Arrays.toString(iArr));
 		
 		System.out.println("다음의 값을 입력하였습니다.");
 		System.out.println(String.join(", ", sArr));
@@ -99,7 +106,7 @@ public class Sample02 {
 
 	public static void main(String[] args) {
 		Sample02 sample = new Sample02();
-		sample.ex04();
+		sample.ex01();
 	}
 
 }
