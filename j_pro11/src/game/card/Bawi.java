2c19704c9bcbcca6478d9db5eb98b34c87e974fd
안병현 <abhyuni@gmail.com>
2022-04-11 11:36:03 +0900

    프로젝트명 변경

--- j_pro11/src/game/card/Bawi.java
+++ j_pro11/src/game/card/Bawi.java
@@ -3,13 +3,33 @@ package game.card;
 public class Bawi extends Hand {
 
 	@Override
-	public boolean compare(Hand otherHand) {
-		return false;
+	public int compare(Hand otherHand) {
+		int result = 0;
+		
+		if(otherHand instanceof Bawi) {
+			result = 0;
+		} else if(otherHand instanceof Bo) {
+			result = -1;
+		} else if(otherHand instanceof Gawi) {
+			result = 1;
+		}
+		
+		return result;
 	}
 
 	@Override
 	public Hand cheat(Hand otherHand) {
-		return null;
+		Hand newHand = null;
+		
+		if(otherHand instanceof Gawi) {
+			newHand = new Bawi();
+		} else if(otherHand instanceof Bawi) {
+			newHand = new Bo();
+		} else if(otherHand instanceof Bo) {
+			newHand = new Gawi();
+		}
+		
+		return newHand;
 	}
 
 }
--- j_pro11/src/game/player/UserPlayer.java
+++ j_pro11/src/game/player/UserPlayer.java
@@ -1,17 +1,79 @@
 package game.player;
 
+import java.util.Random;
+
+import game.card.Bawi;
+import game.card.Bo;
+import game.card.Gawi;
 import game.card.Hand;
+import game.record.Record;
 
 public class UserPlayer implements Player {
-
-	@Override
-	public Hand randomCardHand() {
-		return null;
+	
+	Random rd = new Random();
+	private String name;
+	private Hand hand;
+	private Record record = new Record();
+	
+	public UserPlayer(String name) {
+		this.name = name;
 	}
-
+	
+	public void setCardHand(String name) {
+		if(name.equals("가위")) {
+			hand = new Gawi();
+		} else if(name.equals("바위")) {
+			hand = new Bawi();
+		} else if(name.equals("보")) {
+			hand = new Bo();
+		} else {
+			this.randomCardHand();
+		}
+	}
+	
 	@Override
-	public boolean versus(Hand h1, Hand h2) {
-		return false;
+	public void randomCardHand() {
+		int rand = rd.nextInt(3);
+		switch(rand) {
+			case 0:
+				hand = new Gawi(); break;
+			case 1:
+				hand = new Bawi(); break;
+			case 2:
+				hand = new Bo(); break;
+		}
+	}
+	
+	@Override
+	public String versus(Hand h) {
+		String result = "";
+		switch(hand.compare(h)) {
+			case -1:
+				record.addLose();
+				result = "패"; break;
+			case 0:
+				record.addDraw();
+				result = "무"; break;
+			case 1:
+				record.addWin();
+				result = "승"; break;
+		}
+		return result;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public Hand getHand() {
+		return this.hand;
+	}
+	
+	public String getRecord() {
+		String result = "";
+		result = String.format("%d 승 %d 패 %d 무",
+				record.getWin(), record.getLose(), record.getDraw());
+		return result;
 	}
 
 }
