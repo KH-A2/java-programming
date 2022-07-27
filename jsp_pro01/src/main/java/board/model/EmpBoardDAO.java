25487a2eb7640d8b331402690e4100b7f71028d8
안병현 <abhyuni@gmail.com>
2022-07-27 15:19:11 +0900

    #6
    JSP/Servlet 로 전환함.

---
+++ jsp_pro01/src/main/java/board/model/EmpBoardDAO.java
@@ -0,0 +1,18 @@
+package board.model;
+
+import java.util.*;
+
+import org.apache.ibatis.session.SqlSession;
+
+public class EmpBoardDAO {
+	
+    public EmpBoardDAO() {
+    }
+    
+    private SqlSession session;
+    
+    public boolean insertData(EmpBoardDTO data) {
+        return false;
+    }
+
+}
---
+++ jsp_pro01/src/main/java/board/service/EmpBoardService.java
@@ -0,0 +1,16 @@
+package board.service;
+
+import java.util.*;
+
+import board.model.EmpBoardDTO;
+
+public class EmpBoardService {
+
+    public EmpBoardService() {
+    }
+    
+    public int add(EmpBoardDTO data) {
+        return 0;
+    }
+
+}
